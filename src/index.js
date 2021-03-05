ce4f1666c0c8f950c513aa4ff956db27a8778395
Ilber Pineda <ilberpineda@gmail.com>
2021-03-04 18:02:19 -0800

    logic for new tasks and styling for importance

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import welcome from './modules/loadPage.js';
 // import taskInfo from './modules/addTask.js';
-import createInfo from './modules/addTask.js';
+import createInfo from './modules/createTask.js';
 import appendTask from './modules/addTask.js';
 
 
--- src/modules/addTask.js
+++ src/modules/addTask.js
@@ -1,14 +1,15 @@
 // import taskInfo from './modules/createTask.js';
-import createInfo from './createTask.js';
+// import createInfo from './createTask.js';
 
-// function ResetForm(){
-//     document.getElementById('taskForm').reset()
-// }
+function ResetForm(){
+    document.getElementById('form').reset()
+}
 
-function taskInfo(taskName, dateDue, priority){
+function taskInfo(taskName, dateDue, priority, description){
     this.taskName = taskName.value
-    this.dateDue = dateDue;
-    this.priority = priority
+    this.dateDue = dateDue.value;
+    this.priority = priority.value;
+    this.description = description.value;
 };
 
 let taskList = [];
@@ -17,15 +18,15 @@ let taskDetails;
 function appendTask(){
     event.preventDefault()
     document.getElementById('taskForm').style.display = 'none';
-    taskDetails = new taskInfo(taskName, dateDue, priority)
+    taskDetails = new taskInfo(taskName, dateDue, priority, description)
     taskList.push(taskDetails)
     pushTask()
     storeTask()
-    // ResetForm()
+    ResetForm()
 }
 
 function pushTask(){
-    const taskContainer = document.getElementById('#taskContainer');
+    const taskContainer = document.getElementById('taskContainer');
     const tasks = document.querySelectorAll('.task');
     tasks.forEach(task => taskContainer.removeChild(task))
 
@@ -39,10 +40,69 @@ function makeTask(item){
     task.classList.add('task');
     task.setAttribute('div', taskList.indexOf(item));
 
-    const taskNames = document.createElement('div') 
-    taskNames.textContent = item.taskName
-    taskNames.classList.add('taskName')
-    task.appendChild(taskNames);
+    const taskName = document.createElement('div') 
+    taskName.textContent = item.taskName
+    taskName.classList.add('taskTitle')
+    task.appendChild(taskName);
+
+    const dateDue =  document.createElement('div');
+    // const dateDuDueDay = item.document.getElementById('dateDuDueDue').value
+    dateDue.textContent = item.dateDue
+    dateDue.classList.add('date');
+    task.appendChild(dateDue);
+
+    const priority = document.createElement('div');
+    priority.textContent = item.priority
+    priority.classList.add('priority')
+    task.appendChild(priority);
+
+    if(item.priority === 'not important'){
+        task.style.backgroundColor = 'lightgreen'
+    }else if(item.priority === 'important') {
+        task.style.backgroundColor = 'lightyellow'
+    }else if(item.priority === 'urgent') {
+        task.style.backgroundColor = 'lightcoral'
+    };
+
+    // const description = document.createElement('button')
+    // description.textContent = 'description'
+    // description.classList.add('Description')
+    // task.appendChild(description)
+
+    const descriptionText = document.createElement('details')
+    descriptionText.classList.add('descriptionText')
+    descriptionText.textContent = item.description
+
+    const descriptionInfo = document.createElement('summary')
+    descriptionInfo.textContent = 'Description'
+    descriptionInfo.classList.add('descriptionInfo')
+
+    // description.addEventListener('click', function(){
+    //     event.preventDefault()
+    //     document.getElementById('description').style.display = 'block'
+    // })
+
+    descriptionText.appendChild(descriptionInfo)
+
+    task.appendChild(descriptionText)
+
+    const complete = document.createElement('div')
+    complete.classList.add('complete')
+    task.appendChild(complete)
+
+
+    const completeBtn = document.createElement('button')
+    completeBtn.textContent = 'Done'
+    completeBtn.classList.add('completeBtn');
+    complete.appendChild(completeBtn);
+
+    completeBtn.addEventListener('click', function(){
+        taskList.splice(taskList.indexOf(item), 1);
+        pushTask()
+    })
+
+    
+    
 
     taskContainer.appendChild(task)
 }
@@ -53,10 +113,10 @@ function storeTask(){
 
 
 
-createInfo()
+// createInfo()
 // const module = () => {
-//     const taskFactory = (taskName, dateDue, priority, description) => {
-//         return { taskName, dateDue, priority, description}
+//     const taskFactory = (taskName, dateDuDueDue, priority, description) => {
+//         return { taskName, dateDuDueDue, priority, description}
 //     };
 
 //     let taskArray = []
